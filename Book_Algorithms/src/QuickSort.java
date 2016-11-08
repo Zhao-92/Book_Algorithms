1e334f458b8d45b2e91192b59b0e06089c400f50
Zhao-92 <275426485@qq.com>
2016-11-08 15:37:14 +0800

    更新快排

--- Book_Algorithms/src/QuickSort.java
+++ Book_Algorithms/src/QuickSort.java
@@ -1,19 +1,21 @@
 /**
  * Created by Zcl on 2016/11/7.
+ *
+ * 快速排序，不支持排序数组中有重复元素
  */
 
 public class QuickSort {
     // 如果数组中有两个一样的元素？
     private static int partition(int[] a ,int lo ,int hi){
-        System.out.println("test_part");
         int i = lo+1;
         int j = hi;
         while (i<j){
-            while (less(a,i,lo))
+            while (i < hi && less(a,i,lo))
                 i++;
-            while (less(a,lo,j))
+            while (j >= lo+1 && less(a,lo,j))
                 j--;
-            exch(a,i,j);
+            if(i<j)
+                exch(a,i,j);
         }
         exch(a,lo,j);
         return j;  //返回中间大小的指针
@@ -26,19 +28,13 @@ public class QuickSort {
     }
 
     private static void sort(int[] a , int lo ,int hi){
-        System.out.println("test");
-        while (true){
-            if (lo >= hi)
-                break;
-            int index = partition(a , lo , hi);
-            show(a);
-            sort(a,lo,index-1);   //左排序
-            sort(a,index+1,hi);   //右排序
-        }
-
+        if (lo > hi-1)
+            return;
+        int index = partition(a, lo, hi);
+        sort(a, lo, index - 1);   //左排序
+        sort(a, index + 1, hi);   //右排序
     }
 
-    // i 比 j 小
     private static boolean less(int[] a , int i , int j){
         return a[i]<a[j];
     }
@@ -51,19 +47,16 @@ public class QuickSort {
     private static void isSorted(int[] a){
         int i = 1;
         while (i < a.length)
-            if (a[i-1] > a[i++])
-                System.out.println("Is sorted !");
-            else
-                System.out.println("Not sorted !");
-
+            if (a[i-1] <= a[i++])
+                return;
+        System.out.println("Is sorted !");
     }
 
     public static void main(String[] args){
-        int[] a = {6,1,2,5,3,4};
-        show(a);
+        int[] a = {6,1,2,15,24,16,498,64,54,48,465,32,136,546,86,468,5,3,4};
         sort(a,0,a.length-1);
-//        show(a);
-//        isSorted(a);
+        show(a);
+        isSorted(a);
     }
 
 }
