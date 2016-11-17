3e4ed132a88c602fb2051bde77bdacd41270eb5c
Zhao-92 <275426485@qq.com>
2016-11-17 18:21:53 +0800

    更新算法

---
+++ Book_Algorithms/src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Zcl on 2016/11/17.
+ */
+public class Test {
+    public static void main (String[] args){
+        String str = "abcdefg";
+        System.out.println(str.hashCode());
+    }
+}
